e4bc72be02ccdd85c1689c36cfaf016c30925c6e
PROD_4406729302980856408 <PROD_4406729302980856408@gitea.skywork.ai>
2026-04-14 11:29:38 +0000

    Configure GitHub Pages - Portail variables SOMNUM

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 import path from "path";
 
 export default defineConfig({
-  base: '/',
+  base: '/Portail-variables-somnum.fr/',
   plugins: [
     tailwindcss(),
     react(),
